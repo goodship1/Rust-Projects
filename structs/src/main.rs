c18605c404fbc0e4446a0935e1c18b81601948ad
goodship1 <msgoodship1@gmail.com>
2022-02-21 16:02:44 +0000

    struct concepts

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -10,6 +10,15 @@ struct Rectangle{
     width:u32
 }
 
+impl Rectangle {
+    fn area(&self)->u32{
+        self.length * self.width
+    }
+    fn can_hold(&self,other:Rectangle)->bool{
+        self.width < self.length && other.width < other.length
+    }
+}
+
 fn builduser(name:String,email:String)-> User {
      User {
         name:name,
@@ -18,15 +27,23 @@ fn builduser(name:String,email:String)-> User {
         active:true
     }
 }
-
-fn area(length:u32,width:u32)->u32{
-    length * width
+fn comparerectangles()->bool{
+    let rect_one = Rectangle{length:100,width:200};
+    let rect_two = Rectangle{length:200,width:200};
+    let compare:bool  = rect_one.can_hold(rect_two);
+    compare
 }
+
+
+//fn area(length:u32,width:u32)->u32{
+   // length * width
+//}
 
 
 fn main() {
    let rect_one = Rectangle{ length:100,width:300};
-   let store_area = area(rect_one.length,rect_one.width);
-   println!("area of {}",store_area);
+   //let store_area = area(rect_one.length,rect_one.width);
+   println!("area of {}",rect_one.area());
+   comparerectangles;
 
 }
